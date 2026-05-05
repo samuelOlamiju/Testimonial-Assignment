2bb431da47c64aa84ba79e2286c31f79c3ab4cfd
Olamiju, Oluwaseun S. <105955354+samuelOlamiju@users.noreply.github.com>
2026-05-05 22:06:08 +0100

    Working on Js. Included the array of reviews.

---
+++ Testimonial Assignment/script.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const reviews = [
+  {
+    name: "Samuel Olamiju",
+    job: "FullStack Developer",
+    img: "./images/person-1.jpg",
+    text: "Samuel is a fullstack developer with 3 years experience."
+  },
+  {
+    name: "Susan Smith",
+    job: "Web Developer",
+    img: "./images/person-1.jpg",
+    text: "Susan is a frontend developer with 3 years experience."
+  },
+  {
+      name: "John Doe",
+    job: "Designer",
+    img: "./images/person-2.jpg",
+    text: "John is a UI/UX designer who loves minimal layouts."
+  },
+  {
+    name: "Peter Jones",
+    job: "Intern",
+    img: "./images/person-3.jpg",
+    text: "Peter is an intern learning JavaScript and React."
+  }
+];
